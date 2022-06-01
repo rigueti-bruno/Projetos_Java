41d390214aeef6a7fd346496a6d4f38d1e830fd1
rigueti-bruno <bruno.rigueti@outlook.com>
2022-06-01 14:07:20 -0300

    Teste de commit.

---
+++ PrimeiroProgramaIntelliJ/src/br/com/dio/Calculadora.java
@@ -0,0 +1,19 @@
+package br.com.dio;
+
+public class Calculadora {
+    public static void main(String[] args) {
+        // Variáveis
+        int a = 6;
+        int b = 3;
+
+        // Operações
+        int soma = a + b;
+        int sub = a - b;
+        int mult = a * b;
+        int div = a / b;
+        System.out.println(soma);
+        System.out.println(sub);
+        System.out.println(mult);
+        System.out.println(div);
+    }
+}
